0df4152777094265fe60186cd7f46895fcc8ce97
avepdgk <moczynski93@gmail.com>
2019-09-21 13:14:54 +0200

    php

--- zIinz_4_7s_19 20_Inf_Sp1/test.php
+++ zIinz_4_7s_19 20_Inf_Sp1/test.php
@@ -1,4 +1,17 @@
-test
-
-
-new line 
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>  
+    </title>  
+  </head>
+  <body>
+      <?php
+        $name = 'Maciej';
+        $miasto = 'Poznan';
+         echo $name
+    
+             
+     ?>
+  </body>
+</html>
